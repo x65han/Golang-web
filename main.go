4bb6bdce0cfe1085d4406fb51e620fefd6821f42
Johnson Han <hancanada@hotmail.com>
2016-11-11 22:28:45 -0500

    Dynamically inject html

--- main.go
+++ main.go
@@ -3,22 +3,30 @@ package main
 import (
 	"net/http"
 	"os"
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	port := os.Getenv("PORT")
-
 	if port == "" {port = "5000"}
+
+	// {{template "header.tmpl.html"}}
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	router.LoadHTMLGlob("templates/*.html")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
+	router.GET("/:name", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+		name := c.Params.ByName("name")
+		fmt.Println("Params: ", name);
+	})
+
+	//Run Server
 	router.Run(":" + port)
 }
