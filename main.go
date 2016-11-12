07ce5b482d4d5af58a9f122ad4ced4f9acb0e339
Johnson Han <hancanada@hotmail.com>
2016-11-12 00:08:03 -0500

    favicon.ico

--- main.go
+++ main.go
@@ -10,19 +10,17 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {port = "5000"}
 
-	// {{template "header.tmpl.html"}}
-
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.html")
+	router.LoadHTMLGlob("templates/*.html") 
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
-	router.GET("/:name", func(c *gin.Context) {
+	router.GET("/user/:name", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title":c.Param("name"),
+			"title" : c.Param("name"),
 		})
 	})
 
